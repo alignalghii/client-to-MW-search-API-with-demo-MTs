d281c5e9404a752e3af09288ed065acc99db862a
Endrey Márk <ungazik@gmail.com>
2021-11-30 13:11:41 +0100

    First functioning JSON get and parse

--- Main.hs
+++ Main.hs
@@ -1,37 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-
 module Main where
 
 import Network.Wreq
 
 import Control.Lens
-import GHC.Generics
 import Data.Aeson.Lens (_String, key)
-import Data.Aeson (Value)
--- import Data.Aeson.Types
-
-import Data.Aeson.Internal
+import Data.Aeson (Value, decode, encode)
 
 import Data.Map
 
--- import Prelude hiding (putStrLn)
-import Data.ByteString.Lazy.Char8 as B
+import Prelude hiding (putStrLn)
+import Data.ByteString.Lazy.Char8 (unpack, putStrLn)
 
-import Data.ByteString.Lazy
+-- import Data.ByteString.Lazy (unpack)
 
 type Resp = Response (Map String Value)
 
 main :: IO ()
 main = do
-    resp <- get "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Nelson%20Mandela"
+    resp <- get "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Haskell"
     -- asJSON resp :: IO (Response (Map String Value))
     -- let sc = resp ^. responseStatus . statusCode
-    let bd = resp ^. responseBody
+    let rawBody = resp ^. responseBody
+    let rawBodySimpleString = unpack rawBody
+    let Just ast = decode rawBody :: Maybe Value
+    putStrLn $ encode ast
+    -- let sh = fromJust ma
     -- asJSON resp >>= suck
-    B.putStrLn bd
+    -- putStrLn rawBodySimpleString
+    -- print ast
 
 
 suck :: Response (Map String Value) -> IO ()
