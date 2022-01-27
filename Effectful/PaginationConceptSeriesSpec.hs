a1d118696a11d1a3f32e894b4651c0a01b75fa53
Endrey Márk <ungazik@gmail.com>
2022-01-27 09:29:26 +0100

    Test expectation for the effectful pagination sample (bibliography server)

--- Effectful/PaginationConceptSeriesSpec.hs
+++ Effectful/PaginationConceptSeriesSpec.hs
@@ -11,8 +11,9 @@ import Control.Monad.State.Strict (StateT (StateT))
 
 -- An effectful state transition function for sample.
 -- We build an effect (technically: a *monad*) on top of a pure transition function.
--- Most practical would be building an IO-effect upon it: a server that can be requested only thorough HTTP connection (`IO` monad).
--- But instead of the hard-to-test `IO` monad, we use the simpler `ErrorEffect` monad here for testing.
+-- Most practical would be building an IO-effect upon it:
+-- a ``bibliography server'' that can be requested only thorough HTTP connection (`IO` monad).
+-- But instead of the hard-to-test `IO` monad, we use the simpler `ErrorEffect` monad here for testing:
 
 type BookTitle = String
 
@@ -23,6 +24,14 @@ bibliographyTransitionEffect (Just "5-books-more") = errorfree (["The Hobbit"   
 bibliographyTransitionEffect (Just "2-books-more") = errorfree (["Shrek II"      , "Shrek III"                ], Nothing)
 bibliographyTransitionEffect (Just tkn           ) = withError $ "<<UNRECOGNIZED TOKEN>>[" ++ show tkn ++ "]"
 
+expectedPagesEffect :: ErrorEffect [[BookTitle]]
+expectedPagesEffect = errorfree [
+                                    ["Harry Potter I", "H.P. II" , "H.P. III"     ],
+                                    ["H.P. IV"       , "H.P. V"  , "Lord of rings"],
+                                    ["The Hobbit"    , "Maya bee", "Shrek"        ],
+                                    ["Shrek II"      , "Shrek III"                ]
+                                ]
+
 -- Now here are the tests (specifications) based on the examples above:
 
 spec :: Spec
@@ -30,6 +39,6 @@ spec = do
     describe "Effectful pagination" $ do
         describe "Biliography server" $ do
             it "Without state transformer" $ do
-                pagination_noMT bibliographyTransitionEffect `shouldBe` errorfree [["Harry Potter I", "H.P. II", "H.P. III"], ["H.P. IV", "H.P. V", "Lord of rings"], ["The Hobbit", "Maya bee", "Shrek"], ["Shrek II", "Shrek III"]]
+                pagination_noMT bibliographyTransitionEffect `shouldBe` expectedPagesEffect
             it "With state transformer" $ do
-                pagination_MT (StateT bibliographyTransitionEffect) `shouldBe` errorfree [["Harry Potter I", "H.P. II", "H.P. III"], ["H.P. IV", "H.P. V", "Lord of rings"], ["The Hobbit", "Maya bee", "Shrek"], ["Shrek II", "Shrek III"]]
+                pagination_MT (StateT bibliographyTransitionEffect) `shouldBe` expectedPagesEffect
