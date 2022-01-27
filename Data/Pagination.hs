45e1ecbceaa8d038e4823468c13206ad3b069bc7
Endrey Márk <ungazik@gmail.com>
2022-01-27 10:16:20 +0100

    Differentiate, sort out Data.Transition and Data.Pagination

--- Data/Pagination.hs
+++ Data/Pagination.hs
@@ -1,3 +1,14 @@
-module SampleTypes where
+module Data.Pagination where
+
+import Data.Transition (Transition, TransitionEffect)
+import Control.Monad.State.Strict (State, StateT)
+
 
 type ContinuationToken = String
+
+type PaginationTransition continuationToken page = Transition (Maybe continuationToken) page
+
+type PaginationEffect token effect page  = TransitionEffect (Maybe token) effect page
+
+type PaginationState token page = State (Maybe token) page
+type PaginationStateT token effect page = StateT (Maybe token) effect page
--- Data/Transition.hs
+++ Data/Transition.hs
@@ -1,13 +1,6 @@
-module Aux where
+module Data.Transition where
 
-import Control.Monad.State.Strict (State, StateT (StateT))
 
 type Transition state value = state -> (value, state)
-type PaginationTransition continuationToken page = Transition (Maybe continuationToken) page
 
 type TransitionEffect state effect value = state -> effect (value, state)
-type PaginationEffect token effect page  = TransitionEffect (Maybe token) effect page
-
-type PaginationState token page = State (Maybe token) page
-type PaginationStateT token effect page = StateT (Maybe token) effect page
-
