4b102e46c6e4713efe7ee2f0fc375532647bb513
Endrey Márk <ungazik@gmail.com>
2022-02-10 18:47:21 +0100

    Remove obsolete imports in module `Service.WebServiceConsultation`

--- Service/WebServiceConsultation.hs
+++ Service/WebServiceConsultation.hs
@@ -1,15 +1,13 @@
 module Service.WebServiceConsultation (searchMessage, runSearchFirstPage, runSearchPaged) where
 
-
 import Service.ServiceHigh (printHigh, showHigh, serviceHigh, serviceHigh_withMockBase)
-import Service.ServiceLow (ServiceLow_lengthy, theServiceLow_lengthy, ServiceLow, theServiceLow, serviceFormatErrorMsg)
-import Service.SearchResult (showSearchResult, actSearchResult)
-import Service.InterpretJSON (JSONResponseObject, Title, Sroffset, extractFoundTitlesAndSroffset)
-import Service.Url (SearchPhrase, URL, searchURL)
+import Service.SearchResult (SearchResult, actSearchResult)
+import Service.Url (SearchPhrase, URL)
 
-import Control.Pagination (PaginationEffect)
 import PaginationStateMachines.Effectful.PaginationConceptSeries (pagination_noMT)
 
+import Control.Monad (void)
+import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Maybe (MaybeT, runMaybeT)
 
 
@@ -24,13 +22,9 @@ runSearchFirstPage searchphrase = do
 
 
 runSearchPaged :: SearchPhrase -> IO ()
-runSearchPaged searchphrase = do
-    putStrLn $ "Service: first-page of search result for searchphase `" ++ searchphrase ++ "'"
-    pagination_noMT $ wrapper' theServiceLow_lengthy searchphrase
-    return ()
+runSearchPaged searchphrase = void $ do
+    putStrLn $ "Service: paginated search result for searchphase `" ++ searchphrase ++ "'"
+    runMaybeT $ pagination_noMT $ (mActSearchResult =<<) . serviceHigh searchphrase
 
-wrapper' :: ServiceLow_lengthy -> SearchPhrase -> PaginationEffect Sroffset IO [Title]
-wrapper' service searchphrase maybeSroffset = (actSearchResult . build) =<< service (searchURL searchphrase maybeSroffset)
-
-build :: Maybe JSONResponseObject -> ([Title], Maybe Sroffset)
-build = maybe ([], Nothing) extractFoundTitlesAndSroffset
+mActSearchResult :: SearchResult -> MaybeT IO SearchResult
+mActSearchResult = lift . actSearchResult
