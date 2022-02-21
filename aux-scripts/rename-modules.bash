5fb4bfda0b66f323f5d7b2f76b7d607b5e9a7db5
Endrey Márk <ungazik@gmail.com>
2022-02-21 13:35:24 +0100

    aux sed sript: talking name

--- aux-scripts/rename-modules.bash
+++ aux-scripts/rename-modules.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for modu in `git status | sed -n '/renamed:/s/.*renamed:\s*\(\w*\).*/\1/p'`;
-	do sed -i "/import\|module $modu/s/$modu/Service.$modu/" `grep -l "\(import\|module\) $modu" -nr . --include='*.hs'`;
+for module in `git status | sed -n '/renamed:/s/.*renamed:\s*\(\w*\).*/\1/p'`;
+	do sed -i "/import\|module $module/s/$module/Service.$module/" `grep -l "\(import\|module\) $module" -nr . --include='*.hs'`;
 done;
